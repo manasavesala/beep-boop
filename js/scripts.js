62fd201aea3e550d3a21b5a31ad5907a18118080
Manasa Vesala <vesalamanasa@gmail.com>
2018-10-26 10:30:19 -0700

    added code in branching and added form

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 var beepBoop = function(number) {
+    // var zero = srt.matches(/[^1-9]/);
+    var name = $("#userName").val();
     var array = [];
     for (var i = 0; i <= number; i++){
         array.push(i);
     }
     
     for(var i = 0; i <= array.length; i++){
-
-        if(array[i]%3 === 0){
-            array[i] ='"' + "I'm sorry, Dave. I'm afraid I can't do that." + '"' + " ";
-        }
-        // if(array[i]%){
-            
-        // }
-        if(array[i]%10 === 0){
+        debugger;
+        if(array.includes(0)){
             array[i] = "'Beep!'" + " ";
+        }
+        if(array.includes(1)){
+            array[i] = "'Boop!'" + " ";
+
+        }
+        if(array[i]%3 === 0){
+            array[i] ='"' + "I'm sorry, " +name+". I'm afraid I can't do that." + '"' + " ";
         }
 
 
@@ -27,6 +30,7 @@ $(document).ready(function(){
   $("button").click(function(event){
     event.preventDefault();
     var userNumber = parseInt($("#userNumber").val());
+    var name = $("#userName").val();
     $("#output").text(beepBoop(userNumber));
 
   });
